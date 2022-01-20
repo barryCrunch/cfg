b5061c2accdf0169644195c102e27a4eecd8cfb8
Michael Barry <mbarry@packetdriving.com>
2022-01-19 23:24:24 -0600

    updating from CHJBYT8Y2LT

--- .utilities/update.sh
+++ .utilities/update.sh
@@ -14,22 +14,22 @@ config add $HOME/.utilities/
 config status -uno
 
 config commit -m "updating from ${HOSTNAME}"
+config push origin master
 
-if [[ "$BRANCH" =~ $PROTECTED_BRANCHES ]]; then
-  read p "Are you sure you want to push to \"$BRANCH\" ? (y/n): " -n 1 r < /dev/tty
-  echo
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
-    config push origin master
-    exit 0
-  fi
-  echo "Push aborted."
-  config reset $HOME/.tmux.conf
-  config reset $HOME/.config/lvim/config.lua
-  config reset $HOME/.bashrc
-  config reset $HOME/.bash_aliases
-  config reset $HOME/.utilities/
-  exit 1
-fi
-exit 0
+# if [[ "$BRANCH" =~ $PROTECTED_BRANCHES ]]; then
+#   read p "Are you sure you want to push to \"$BRANCH\" ? (y/n): " -n 1 r < /dev/tty
+#   echo
+#   if [[ $REPLY =~ ^[Yy]$ ]]; then
+#     exit 0
+#   fi
+#   echo "Push aborted."
+#   config reset $HOME/.tmux.conf
+#   config reset $HOME/.config/lvim/config.lua
+#   config reset $HOME/.bashrc
+#   config reset $HOME/.bash_aliases
+#   config reset $HOME/.utilities/
+#   exit 1
+# fi
+# exit 0
 
 
